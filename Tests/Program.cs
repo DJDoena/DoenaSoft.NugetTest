2dc38f83bb19eb949e1b0b77086fea11e898ffbd
DJDoena <github@doena-soft.de>
2023-08-25 15:15:14 +0200

    1.0.11

---
+++ Tests/Program.cs
@@ -0,0 +1,8 @@
+﻿internal static class Program
+{
+    private static void Main()
+    {
+        System.Console.WriteLine(DoenaSoft.NugetTest.Hello.World());
+        System.Console.ReadLine();
+    }
+}
